826b6cb3b66beecff857ca7b4984a0a43cfec62f
Fatima <2021320856@ifam.edu.br>
2023-08-04 22:04:32 -0400

    Aula 01 Finalizada, apenas aula teórica

---
+++ Aula-01/src/main/java/AnotacoesAula.java
@@ -0,0 +1,77 @@
+public class AnotacoesAula {
+
+/*
+Requisitos Fundamentais
+
+Para o bom de uso de componentes:
+Ter uma de componentes catalogados e documentados;
+Componentes devem ser confiáveis;
+Documentação associada com onde eles foram utilizados e problemas de reúso.
+
+Benefícios
+
+Maior Confiabilidade
+Redução dos riscos de processo
+Uso efetivo de especialistas
+Um sênior pode criar um componente confiável para outros com menos experiência poderem usar.
+Conformidade de padrões
+Os componentes precisam ter padrões definidos, eu não posso criar um componente que foge dos padrões daquele setor.
+Desenvolvimento acelerado
+O trabalho que era feito em uma semana é reduzido a minutos.
+
+Problemas
+
+Aumento dos Custos de Manutenção: se o código fonte não estiver disponível;
+Às vezes é necessário fazer alterações nos componentes de terceiros e não se tem acesso ao código fonte para depender deles, então é necessário realizar uma solicitação, o que sai muito caro.
+Falta de ferramentas de apoio: Algumas ferramentas CASES não permitem adicionar componentes de terceiros. Ex: Maker ALL.
+Síndrome do “Não foi inventado aqui”. Mais confiável e desafiador.
+Tem gente que prefere trabalhar de forma manual e não confia em componentes pré-montados.
+Manutenção de uma biblioteca de componentes.
+Encontrar e adaptar componentes: encontrar, entendidos e adaptados. Certeza que podem encontrar antes de começar a procurar.
+Às vezes é necessário encontrar componentes com código fonte disponível para adaptar. Dessa forma, sai muito mais barato o desenvolvimento.
+
+Uso de Geradores de Programa
+
+Caso bem disseminado de uso de componentes
+Visual Basic, Delphi
+Maker Al
+IDE NetBeans e Eclipse: Componentes e Plugin
+
+Desenvolvimento Baseado em Componentes
+
+Componentes como provedor de serviços
+Entidade executável independente
+Interfaces
+Operações parametrizadas e estado interno nunca exposto
+2 interfaces: provides e requires
+
+Necessidade VS Querer
+
+No desenvolvimento orientado a componentes não são os requisitos que mandam, mas sim os componentes que podem definir os requisitos. Pois a componentização reduz os custos, então pode ser melhor mudar os requisitos para se adaptar as componentes do que desenvolver do zero e gastar mais tempo e dinheiro.
+
+Roger Pressman:
+
+Uma parte não trivial de um sistema, praticamente independente e substituível.
+
+Evolução do Modelo de Componentes
+
+Funções e Procedimentos (Uma função pode se relacionar a diversas classes do meu programa)
+RPC - Remote Procedure Call (Chamada de Procedimento Remoto)
+Interfaces Corba - Heterogeneidade (Diferentes Tecnologias se Comunicando)
+Faz chamadas remotas e os elementos envolvidos na chamada não precisam ser da mesma tecnologia. Sendo assim, é possível utilizar um servidor em Cobal que se comunica com um utilizando a C. Passavam essas chamadas por outro software que realizava o gerenciamento da integração, assim adaptando a comunicação conforme a necessidade das tecnologias.
+OBS: Primeira tecnologia a resolver a comunicação entre tecnologias diferentes.
+Components
+Um componente que resolve a comunicação para diferentes plataformas, por exemplo.
+Web Service
+São serviços que oferecem soluções para sua aplicação, assim você pode conectar diferentes serviços para criar soluções inovadoras. O consumo de APIs para saber o clima, envio de e-mails automatizados em um formulário ou mesmo para conectar alguém a uma outra plataforma em sua aplicação.
+
+Mãozinha aberta significa que está pedindo a integração.
+Mãozinha fechada você está oferecendo o serviço.
+
+Problemas de Dependências
+
+Serviços podem ficar indisponíveis ou falharem, então é necessário ter um plano B. Assim, é necessário pensar em alternativas para caso os serviços consumidos fiquem indisponíveis para que sua aplicação fique inutilizável. Imagine o impacto de ter um e-commerce gigante com um serviço dos correios para calcular o frete indisponível, isso pode causar muitas perdas.
+
+ */
+
+}
