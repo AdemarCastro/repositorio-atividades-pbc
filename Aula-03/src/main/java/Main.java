0f15d8ea713da6a7be12bd4bdf78856e7d769bb6
AdemarCastro <2022002444@ifam.edu.br>
2023-08-11 21:26:52 -0400

    Novo commit

--- Aula-03/src/main/java/Main.java
+++ Aula-03/src/main/java/Main.java
@@ -1,7 +1,12 @@
+import controller.HomeController;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Oi");
+
+        HomeController homeController = new HomeController();
+        homeController.executar();
+
     }
 
 }
